8e40c40e94df9716e681ce9d22a93ebad5b47040
vighnesh153 <vighnesh.raut13@gmail.com>
2020-08-02 07:31:22 +0000

    Deploying to gh-pages from  @ 65aea92df17bcdba03cc8674daae5ff15d3e7b37 🚀

---
+++ precache-manifest.0f3125dd51c9802caf1528388edcea47.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336e5b791d7f01460a14964c303bccb4",
+    "url": "/canvas-api-illustrations/index.html"
+  },
+  {
+    "revision": "6f2522114de16d3bb521",
+    "url": "/canvas-api-illustrations/static/css/main.0dc1868b.chunk.css"
+  },
+  {
+    "revision": "1a539b887b9ce227a42f",
+    "url": "/canvas-api-illustrations/static/js/0.4124afe2.chunk.js"
+  },
+  {
+    "revision": "93b7ad4d3bf222efd0d2",
+    "url": "/canvas-api-illustrations/static/js/1.27702143.chunk.js"
+  },
+  {
+    "revision": "dfa63c4a5231b5468c57",
+    "url": "/canvas-api-illustrations/static/js/10.afe094e4.chunk.js"
+  },
+  {
+    "revision": "b08b9e0cb0f87757f05e",
+    "url": "/canvas-api-illustrations/static/js/11.c22708f6.chunk.js"
+  },
+  {
+    "revision": "99debdaf646c36f1ffe4",
+    "url": "/canvas-api-illustrations/static/js/12.9c25ba7e.chunk.js"
+  },
+  {
+    "revision": "13afbf450d556afeb3f6",
+    "url": "/canvas-api-illustrations/static/js/13.a98eb98b.chunk.js"
+  },
+  {
+    "revision": "7dd769f32cbdd7318178",
+    "url": "/canvas-api-illustrations/static/js/14.dd68b01b.chunk.js"
+  },
+  {
+    "revision": "9036dec0a113b3977f43",
+    "url": "/canvas-api-illustrations/static/js/15.ba300a73.chunk.js"
+  },
+  {
+    "revision": "b576a1e3faf4e6de6be2",
+    "url": "/canvas-api-illustrations/static/js/16.8b0d0260.chunk.js"
+  },
+  {
+    "revision": "68eda4b41f613767a2db",
+    "url": "/canvas-api-illustrations/static/js/17.8c89514d.chunk.js"
+  },
+  {
+    "revision": "f965a148543e35036b65",
+    "url": "/canvas-api-illustrations/static/js/18.8403d007.chunk.js"
+  },
+  {
+    "revision": "63d103b6b49980811be6",
+    "url": "/canvas-api-illustrations/static/js/19.f7a7f6f2.chunk.js"
+  },
+  {
+    "revision": "20bdaee2e33825e1b803",
+    "url": "/canvas-api-illustrations/static/js/2.efb55ab6.chunk.js"
+  },
+  {
+    "revision": "e87635352d687acce234",
+    "url": "/canvas-api-illustrations/static/js/20.0eb7f299.chunk.js"
+  },
+  {
+    "revision": "883a36f4efcc239f12a9",
+    "url": "/canvas-api-illustrations/static/js/21.0825b220.chunk.js"
+  },
+  {
+    "revision": "9e379919c21c024226ca",
+    "url": "/canvas-api-illustrations/static/js/22.a7f226b7.chunk.js"
+  },
+  {
+    "revision": "39b7f587d139f42c4407",
+    "url": "/canvas-api-illustrations/static/js/23.5f45e57c.chunk.js"
+  },
+  {
+    "revision": "851a0b98d76d06e4c326",
+    "url": "/canvas-api-illustrations/static/js/24.3ef3f63f.chunk.js"
+  },
+  {
+    "revision": "f574d5ab2d1527571673",
+    "url": "/canvas-api-illustrations/static/js/25.bdaa4761.chunk.js"
+  },
+  {
+    "revision": "063a2e3282e5cfcd9787",
+    "url": "/canvas-api-illustrations/static/js/26.4760cb6b.chunk.js"
+  },
+  {
+    "revision": "cc054797693dbd829eda",
+    "url": "/canvas-api-illustrations/static/js/27.21da1ad1.chunk.js"
+  },
+  {
+    "revision": "88b800a9580d6ca22464",
+    "url": "/canvas-api-illustrations/static/js/28.c8bd4726.chunk.js"
+  },
+  {
+    "revision": "88af66d451e79bad8c45",
+    "url": "/canvas-api-illustrations/static/js/29.5ad4e0f3.chunk.js"
+  },
+  {
+    "revision": "85ace3f8517737b435c7",
+    "url": "/canvas-api-illustrations/static/js/3.52699a2c.chunk.js"
+  },
+  {
+    "revision": "0da18bb55bb8085a301c",
+    "url": "/canvas-api-illustrations/static/js/30.a714d377.chunk.js"
+  },
+  {
+    "revision": "a3e3202abdfd6dc303d0",
+    "url": "/canvas-api-illustrations/static/js/31.25a81163.chunk.js"
+  },
+  {
+    "revision": "6280c480487ae9a1e8b5",
+    "url": "/canvas-api-illustrations/static/js/32.8501ae49.chunk.js"
+  },
+  {
+    "revision": "c7650c76b0e030b05a0c",
+    "url": "/canvas-api-illustrations/static/js/33.0d2f2d4a.chunk.js"
+  },
+  {
+    "revision": "e7fa2207f5945100c41b",
+    "url": "/canvas-api-illustrations/static/js/34.53a8a6a9.chunk.js"
+  },
+  {
+    "revision": "8b93b32a41e416fe1e9c",
+    "url": "/canvas-api-illustrations/static/js/35.cfa1336f.chunk.js"
+  },
+  {
+    "revision": "bbae6b88211aeba6a115",
+    "url": "/canvas-api-illustrations/static/js/36.4c09a55d.chunk.js"
+  },
+  {
+    "revision": "283b2209719c9ab2a30f",
+    "url": "/canvas-api-illustrations/static/js/37.b91894f0.chunk.js"
+  },
+  {
+    "revision": "fc133e0e189e5e070ac0",
+    "url": "/canvas-api-illustrations/static/js/38.843d232f.chunk.js"
+  },
+  {
+    "revision": "a2c0c4287f452c3989fd",
+    "url": "/canvas-api-illustrations/static/js/39.9f25a491.chunk.js"
+  },
+  {
+    "revision": "c5d7f3ac28e352cfb7d6",
+    "url": "/canvas-api-illustrations/static/js/4.77999d5b.chunk.js"
+  },
+  {
+    "revision": "bf8bbe95929b4dcf9a73",
+    "url": "/canvas-api-illustrations/static/js/40.5577d847.chunk.js"
+  },
+  {
+    "revision": "1e329b7e5830eaf10ce1",
+    "url": "/canvas-api-illustrations/static/js/41.358e7ec8.chunk.js"
+  },
+  {
+    "revision": "ddfada16ca20f0c2b1d3",
+    "url": "/canvas-api-illustrations/static/js/42.a6715237.chunk.js"
+  },
+  {
+    "revision": "da7b2b436c978529b6d5",
+    "url": "/canvas-api-illustrations/static/js/43.6e34f6df.chunk.js"
+  },
+  {
+    "revision": "c4418de170c121e437ac",
+    "url": "/canvas-api-illustrations/static/js/44.83bb90f7.chunk.js"
+  },
+  {
+    "revision": "2a5ec7dac88a51d9c144",
+    "url": "/canvas-api-illustrations/static/js/45.92413d68.chunk.js"
+  },
+  {
+    "revision": "6d0ffc82bc0da36a85cf",
+    "url": "/canvas-api-illustrations/static/js/46.33cd6cbb.chunk.js"
+  },
+  {
+    "revision": "1b79b18546095367d8bc",
+    "url": "/canvas-api-illustrations/static/js/47.ec3cce33.chunk.js"
+  },
+  {
+    "revision": "8a2e43e62c4535abb0c2",
+    "url": "/canvas-api-illustrations/static/js/48.5d463984.chunk.js"
+  },
+  {
+    "revision": "3a4ab73eac3d84d10f27",
+    "url": "/canvas-api-illustrations/static/js/49.3c064cb3.chunk.js"
+  },
+  {
+    "revision": "e36054eae6e85943b426",
+    "url": "/canvas-api-illustrations/static/js/5.7731d230.chunk.js"
+  },
+  {
+    "revision": "2a99180adb1275655087",
+    "url": "/canvas-api-illustrations/static/js/52.f5549a97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/canvas-api-illustrations/static/js/52.f5549a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba8cc3ac827fd07762c",
+    "url": "/canvas-api-illustrations/static/js/53.cf2435fd.chunk.js"
+  },
+  {
+    "revision": "45ec43f3c25eac9623be",
+    "url": "/canvas-api-illustrations/static/js/6.acc74b5e.chunk.js"
+  },
+  {
+    "revision": "ab5be9a13671c71acf55",
+    "url": "/canvas-api-illustrations/static/js/7.8f87cd69.chunk.js"
+  },
+  {
+    "revision": "bd2c25d727cd0563255b",
+    "url": "/canvas-api-illustrations/static/js/8.13631f63.chunk.js"
+  },
+  {
+    "revision": "4829e5baf49ad5c05978",
+    "url": "/canvas-api-illustrations/static/js/9.f2f12901.chunk.js"
+  },
+  {
+    "revision": "6f2522114de16d3bb521",
+    "url": "/canvas-api-illustrations/static/js/main.5a6492b3.chunk.js"
+  },
+  {
+    "revision": "7acad861d52ce30ee0aa",
+    "url": "/canvas-api-illustrations/static/js/runtime-main.bb817279.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93b26ae118fbf6d145d75f8ae2b5941a.js"
+  "/canvas-api-illustrations/precache-manifest.0f3125dd51c9802caf1528388edcea47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/canvas-api-illustrations/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
