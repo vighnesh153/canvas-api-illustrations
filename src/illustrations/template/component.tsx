1b683d1c0026a8ad7839a942e25ea8bfa85ffbaa
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-03-07 07:48:36 +0530

    update illustrations-template

--- src/illustrations/template/component.tsx
+++ src/illustrations/template/component.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import DataAttributes from "src/models/data-attributes";
+
+import Controller from "./controller";
 
 type PropsType = {
   canvasRef: React.RefObject<HTMLCanvasElement>;
@@ -8,6 +10,17 @@ type PropsType = {
 
 function Component(props: PropsType) {
   const { canvasRef, setDataAttributes } = props;
+
+  useEffect(() => {
+    const canvasEl = props.canvasRef.current as HTMLCanvasElement;
+    const gameController = new Controller(canvasEl);
+    canvasEl.focus();
+    gameController.start();
+
+    return () => {
+      gameController.stop();
+    };
+  }, []);
 
   return <></>;
 }
--- src/illustrations/template/controller.ts
+++ src/illustrations/template/controller.ts
@@ -1,3 +1,23 @@
-// Just to make TS happy. Remove when
-// inserting something in this file.
-export default () => {};
+import Canvas from "src/models/canvas";
+
+export default class Controller {
+  private readonly canvas: Canvas;
+  private interval = setTimeout(() => {}, 0);
+
+  constructor(e: HTMLCanvasElement) {
+    this.canvas = new Canvas(e);
+  }
+
+  start(): void {
+    this.interval = setInterval(this.draw.bind(this), 16);
+  }
+
+  stop(): void {
+    clearInterval(this.interval);
+    clearInterval(this.interval);
+  }
+
+  draw(): void {
+
+  }
+}
